9f07a8be6f3ff6d7873b023c4dc7c332960b0b40
Ilia Bubnov <makebreathe1224@gmail.com>
2025-07-30 11:58:50 +0200

    created models and adjusted them to fit in with checks

---
+++ server/src/models/Follow.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import validateAllowedFields from "../util/validateAllowedFields.js";
+
+const followSchema = new Schema({
+  follower: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  following: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+// adding compound index for the unique follower-following pairs
+followSchema.index({ follower: 1, following: 1 }, { unique: true });
+
+const Follow = mongoose.model("follows", followSchema);
+
+export const validateFollow = (followObject) => {
+  const errorList = [];
+  const allowedKeys = ["follower", "following"];
+
+  const validatedKeysMessage = validateAllowedFields(followObject, allowedKeys);
+
+  if (validatedKeysMessage.length > 0) {
+    errorList.push(validatedKeysMessage);
+  }
+
+  if (followObject.follower == null) {
+    errorList.push("follower is a required field");
+  }
+
+  if (followObject.following == null) {
+    errorList.push("following is a required field");
+  }
+
+  return errorList;
+};
+
+export default Follow;
---
+++ server/src/models/Like.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import validateAllowedFields from "../util/validateAllowedFields.js";
+
+const likeSchema = new Schema({
+  created_at: { type: Date, default: Date.now },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+});
+
+const Like = mongoose.model("Like", likeSchema);
+
+export const validateLike = (likeObject) => {
+  const errorList = [];
+  const allowedKeys = ["created_at", "user", "post"];
+
+  const validatedKeysMessage = validateAllowedFields(likeObject, allowedKeys);
+
+  if (validatedKeysMessage.length > 0) {
+    errorList.push(validatedKeysMessage);
+  }
+
+  if (likeObject.created_at == null) {
+    errorList.push("created_at is a required field");
+  }
+
+  if (likeObject.user == null) {
+    errorList.push("user is a required field");
+  }
+
+  if (likeObject.post == null) {
+    errorList.push("post is a required field");
+  }
+
+  return errorList;
+};
+
+export default Like;
