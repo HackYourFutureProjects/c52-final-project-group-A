91a9cdf9c83725ac3b3aac195ea19cced8c5b666
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-07-31 13:34:27 +0200

    the error with incorrect importing the data awas fixed

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -1,90 +1,89 @@
-// import { Post } from "../models/Post.js"; // TODO update the path after model is setted
-// // import jwt from "jsonwebtoken";
+import Post from "../models/Post.js";
+//import jwt from "jsonwebtoken";
 
-// //
-// export const getAllPosts = async (req, res) => {
-//   try {
-//     const posts = await Post.find().populate("author", "username email");
-//     res.json(posts);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author", "username email");
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-// // Get Post By ID endpoint
-// export const getPostById = async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.id).populate(
-//       "author",
-//       "username email",
-//     );
-//     if (!post) return res.status(404).json({ message: "Post not found" });
-//     res.json(post);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+// Get Post By ID endpoint
+export const getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate(
+      "author",
+      "username email",
+    );
+    if (!post) return res.status(404).json({ message: "Post not found" });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-// // Create Post endpoint
-// export const createPost = async (req, res) => {
-//   try {
-//     const { title, content, status } = req.body;
+// Create Post endpoint
+export const createPost = async (req, res) => {
+  try {
+    const { title, content, status } = req.body;
 
-//     const newPost = await Post.create({
-//       title,
-//       content,
-//       status,
-//       author: req.user._id,
-//     });
+    const newPost = await Post.create({
+      title,
+      content,
+      status,
+      author: req.user._id,
+    });
 
-//     res.status(201).json(newPost);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// };
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-// // Update Post endpoint
-// export const updatePost = async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.id);
-//     if (!post) return res.status(404).json({ message: "Post not found" });
+// Update Post endpoint
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
 
-//     // Check that only the author can edit
-//     if (post.author.toString() !== req.user._id.toString()) {
-//       return res
-//         .status(403)
-//         .json({ message: "Not authorized to update this post" });
-//     }
+    // Check that only the author can edit
+    if (post.author.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to update this post" });
+    }
 
-//     const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//       runValidators: true,
-//     });
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-//     res.json(updatedPost);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// };
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-// // Delete Post endpoint
-// export const deletePost = async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.id);
+// Delete Post endpoint
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
-//     if (!post) return res.status(404).json({ message: "Post not found" });
+    if (!post) return res.status(404).json({ message: "Post not found" });
 
-//     // Compare the author with the current user
-//     if (post.author.toString() !== req.user._id.toString()) {
-//       return res
-//         .status(403)
-//         .json({ message: "Not authorized to delete this post" });
-//     }
+    // Compare the author with the current user
+    if (post.author.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to delete this post" });
+    }
 
-//     await post.deleteOne();
+    await post.deleteOne();
 
-//     res.json({ message: "Post deleted" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+    res.json({ message: "Post deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
