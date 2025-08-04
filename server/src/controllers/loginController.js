6262a93fb2f688e88383557544a41335e855f1e3
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-08-04 16:32:28 +0200

    Copilot fixes

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -33,7 +33,9 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
     });
 
     return res.status(200).json({
