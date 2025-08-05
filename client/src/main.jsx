4cdfb2f3a183d9aff2e0940d7d6fcff629362a04
Ilia Bubnov <makebreathe1224@gmail.com>
2025-08-05 18:32:35 +0200

    removed getEnv and config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,10 @@ import "./index.css";
 import App from "./App.jsx";
 import AppWrapper from "./AppWrapper";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import config from "./config.js";
-
-const { CLIENT_ID } = config;
 
 createRoot(document.getElementById("root")).render(
   <AppWrapper>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </AppWrapper>,
--- client/src/util/getEnv.js
+++
@@ -1,7 +0,0 @@
-function getEnv(variable, defaultValue, transformFn = (val) => val) {
-  const value = import.meta.env[variable];
-  if (value === undefined || value === null) return defaultValue;
-  return transformFn(value);
-}
-
-export default getEnv;
