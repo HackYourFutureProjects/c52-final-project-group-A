e9371212da2505ca2b0a681078f9e8c9a53aad18
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-08-05 12:52:16 +0200

    Fix Express 5 catch-all route for SPA

--- server/src/index.js
+++ server/src/index.js
@@ -53,7 +53,7 @@ const startServer = async () => {
 
     if (NODE_ENV === "production") {
       app.use(express.static(path.join(__dirname, "../../client/dist")));
-      app.get("/*", (req, res) => {
+      app.get("/:path(*)", (req, res) => {
         res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
       });
     }
