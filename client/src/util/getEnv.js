afef2f5a6f0ed2efa859a445ec700e9bc478e0ce
Ilia Bubnov <makebreathe1224@gmail.com>
2025-08-05 17:22:21 +0200

    testing build on Heroku to allow for loading .env variables

--- client/src/util/getEnv.js
+++ client/src/util/getEnv.js
@@ -1,9 +1,7 @@
 function getEnv(variable, transformFn = (val) => val) {
   const value = import.meta.env[variable];
-  if (value === undefined || value === null)
-    throw new Error(`Environment variable ${variable} is not defined.`);
-  if (transformFn) return transformFn(value);
-  return value;
+  if (value === undefined || value === null) return;
+  return transformFn(value);
 }
 
 export default getEnv;
