f640294f20886f7e3d661126842d4ef9820d9ac8
Ilia Bubnov <makebreathe1224@gmail.com>
2025-08-05 18:23:57 +0200

    still fixing env

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,8 @@
 import getEnv from "./util/getEnv.js";
 
 const config = {
-  BACKEND_URL: getEnv("VITE_BACKEND_URL", "http://localhost:3000"),
-  CLIENT_ID: getEnv("VITE_CLIENT_ID", "defaultClientId"),
+  BACKEND_URL: getEnv("VITE_BACKEND_URL", "http://localhost:3000", console.log),
+  CLIENT_ID: getEnv("VITE_CLIENT_ID", "defaultClientId", console.log),
 };
 
 export default config;
