2f1fa42451e2e4752319e20a66bca894dc99f3bf
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-08-07 17:04:05 +0200

    home page temporary updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <>Home Page</>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        <Link to="/test">Temporary test link for Posts</Link>
+      </p>
+    </>
+  );
 }
 
 export default Home;
