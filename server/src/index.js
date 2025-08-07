d2fdb65a0c72c987363c9cd7301b40a3a4bb2609
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-08-07 15:49:43 +0200

    Test: Minimal server deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,44 +1,9 @@
 import express from "express";
-import app from "./app.js";
-import { logInfo, logError } from "./util/logging.js";
-import connectDB from "./db/connectDB.js";
-import testRouter from "./testRouter.js";
-import config from "./config.js";
+const app = express();
 
-const { PORT, NODE_ENV } = config;
+// ONLY FOR TEST
+app.get("/", (req, res) => res.send("OK"));
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      logInfo(`Server started on port ${PORT}`);
-    });
-  } catch (error) {
-    logError(error);
-  }
-};
-
-/****** Host our client code for Heroku *****/
-/**
- * We only want to host our client code when in production mode as we then want to use the production build that is built in the dist folder.
- * When not in production, don't host the files, but the development version of the app can connect to the backend itself.
- */
-if (NODE_ENV === "production") {
-  app.use(
-    express.static(new URL("../../client/dist", import.meta.url).pathname),
-  );
-  // Redirect * requests to give the client data
-  app.get("/*file", (req, res) =>
-    res.sendFile(
-      new URL("../../client/dist/index.html", import.meta.url).pathname,
-    ),
-  );
-}
-
-/****** For cypress we want to provide an endpoint to seed our data ******/
-if (NODE_ENV !== "production") {
-  app.use("/api/test", testRouter);
-}
-
-// Start the server
-startServer();
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
