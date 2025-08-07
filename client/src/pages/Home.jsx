cadd81c15a26b4764fbb04e90aa55e059e45ad8e
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-08-07 17:13:40 +0200

    temporary added link on Landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,5 @@
-import { Link } from "react-router-dom";
-
 function Home() {
-  return (
-    <>
-      <h1>Home Page</h1>
-      <p>
-        <Link to="/test">Temporary test link for Posts</Link>
-      </p>
-    </>
-  );
+  return <>Home Page</>;
 }
 
 export default Home;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Landing() {
-  return <>Landing Page</>;
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Landing Page</h1>
+      <Link to="/test">temporary page for Posts</Link>
+    </div>
+  );
 }
 
 export default Landing;
