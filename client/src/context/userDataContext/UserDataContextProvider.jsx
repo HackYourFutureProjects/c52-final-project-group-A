473889a8f6313f3639524de4572df292a48ddff9
Ilia Bubnov <makebreathe1224@gmail.com>
2025-08-09 11:17:32 +0200

    added global context for userData

---
+++ client/src/context/userDataContext/UserDataContextProvider.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import userDataContext from "./userDataContext";
+import PropTypes from "prop-types";
+
+const UserDataContextProvider = ({ children }) => {
+  const [userData, setUserData] = useState();
+
+  return (
+    <userDataContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </userDataContext.Provider>
+  );
+};
+
+UserDataContextProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default UserDataContextProvider;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,18 @@
 import ProfileDash from "../components/ProfileDash/ProfileDash.jsx";
+import { useContext } from "react";
+import UserDataContext from "../context/userDataContext/UserDataContext.js";
 
 function Profile() {
+  const { user } = useContext(UserDataContext);
+  console.log(user);
+
+  if (!user) {
+    console.log("You are not logged in");
+  }
+
   return (
     <>
-      <ProfileDash size="lg" />
+      <ProfileDash size="lg" user={user} />
       <ProfileDash size="sm" />
     </>
   );
