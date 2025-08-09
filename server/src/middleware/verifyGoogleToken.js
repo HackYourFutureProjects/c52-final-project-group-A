ae2a38dd0d36ff2fe356b26dc9844594e9d713e0
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-08-09 10:57:38 +0200

    fixes based on comments

--- server/src/middleware/verifyGoogleToken.js
+++ server/src/middleware/verifyGoogleToken.js
@@ -1,5 +1,5 @@
 import { logError } from "../util/logging.js";
-import generateUsername from "../util/usernameGenerator.js";
+import { generateUsername } from "../util/usernameGenerator.js";
 import { validateUser } from "../models/User.js";
 
 export const verifyGoogleToken = async (req, res, next) => {
@@ -12,14 +12,20 @@ export const verifyGoogleToken = async (req, res, next) => {
   }
 
   try {
-    const response = await fetch(
-      "https://www.googleapis.com/oauth2/v2/userinfo",
-      {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 5000); // 5 seconds timeout
+    let response;
+
+    try {
+      response = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
-      },
-    );
+        signal: controller.signal,
+      });
+    } finally {
+      clearTimeout(timeout);
+    }
 
     if (!response.ok) {
       return res.status(401).json({ msg: "Invalid access token" });
