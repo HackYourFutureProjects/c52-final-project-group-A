0666637d6d40c56558e2dc421796c82f51856794
Ilya Bubnov <makebreathe1224@gmail.com>
2025-08-16 12:21:25 +0200

    fixes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,19 +8,25 @@ function Profile() {
   const { userData } = useContext(UserDataContext);
   const [posts, setPosts] = useState([]);
 
-  const { performFetch } = useFetch(`/post/${userData?._id}`, (response) => {
+  const { performFetch, error } = useFetch("", (response) => {
     setPosts(response.result);
   });
 
   useEffect(() => {
+    if (!userData?._id) return;
     const options = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
+      url: `/post/user/${userData._id}`,
     };
     performFetch(options);
-  }, []);
+  }, [userData?._id]);
+
+  if (error) {
+    console.error(error);
+  }
 
   return (
     <>
