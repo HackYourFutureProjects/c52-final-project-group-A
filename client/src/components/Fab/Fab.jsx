600fd391189bd6233c6c429b65c31391a7d30f56
Ilya Bubnov <makebreathe1224@gmail.com>
2025-08-26 10:20:53 +0200

    fixed issues

--- client/src/components/Fab/Fab.jsx
+++ client/src/components/Fab/Fab.jsx
@@ -1,10 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import style from "./Fab.module.css";
-import PropTypes from "prop-types";
 import Button from "../Button.jsx";
 import PlusIcon from "../icons/PlusIcon.jsx";
 
-export default function Fab() {
+function Fab() {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -18,6 +17,4 @@ export default function Fab() {
   );
 }
 
-Fab.propTypes = {
-  children: PropTypes.node,
-};
+export default Fab;
