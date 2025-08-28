26e7159c530e663c8b1408dce46fd83f4983554d
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-08-28 17:20:10 +0200

    edit proflie button

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,10 @@
+import { useEffect, useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import ProfileDash from "../../components/ProfileDash/ProfileDash.jsx";
-import { useEffect, useState } from "react";
 import Post from "../../components/Post/Post.jsx";
 import useFetch from "../../hooks/useFetch.js";
-import { useParams } from "react-router-dom";
-import { useContext } from "react";
 import StateContext from "../../context/state/StateContext.js";
+import Button from "../../components/Button.jsx";
 import style from "./Profile.module.css";
 
 function Profile() {
@@ -18,27 +18,34 @@ function Profile() {
   });
 
   useEffect(() => {
-    const options = {
+    performFetch({
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    performFetch(options);
+      headers: { "Content-Type": "application/json" },
+    });
   }, [username]);
+
+  const navigate = useNavigate();
 
   if (error) {
     console.error(error);
   }
 
   return (
-    <main className={style.main}>
+    <main>
       <ProfileDash size="lg" user={userData} followBtn={!isUser} />
-      <div className={style.postsContainer}>
-        {userData &&
-        Array.isArray(userData.posts) &&
-        userData.posts.length > 0 ? (
-          userData?.posts?.map((post) => <Post key={post._id} post={post} />)
+      {isUser && (
+        <div className={style.editProfileBtnWrap}>
+          <Button
+            className={style.editProfileBtn}
+            onClick={() => navigate(`/user/${username}/edit`)}
+          >
+            Edit Profile
+          </Button>
+        </div>
+      )}
+      <div>
+        {userData?.posts?.length > 0 ? (
+          userData.posts.map((post) => <Post key={post._id} post={post} />)
         ) : (
           <p>No posts found.</p>
         )}
