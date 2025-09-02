7d49e4a2ec8bba6d97c5375a14140183dfa70599
Ilya Bubnov <makebreathe1224@gmail.com>
2025-09-02 09:04:02 +0200

    fixes

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -6,11 +6,7 @@ function Loading({ x, y }) {
   // using inline styles because of the limitation of the library/ requirements for reactivity
   return (
     <div className={style.wrapper} style={{ top: y, left: x }}>
-      <HashLoader
-        size={30}
-        color="#fe4a22"
-        cssOverride={{ width: "0", height: "0" }}
-      />
+      <HashLoader size={30} color="#fe4a22" />
     </div>
   );
 }
