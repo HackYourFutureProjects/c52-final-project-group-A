28da86f82ed25a0321febe019dc67179759b05f1
Ilya Bubnov <makebreathe1224@gmail.com>
2025-09-03 18:31:29 +0200

    working on rebasing

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
-import StateContext from "../context/state/StateContext.js";
-import useFetchWithAuth from "../hooks/useFetchWithAuth.js";
 import EditPostForm from "../components/EditPostForm/EditPostForm.jsx";
-import useSetError from "../hooks/useSetError.js";
+import UserContext from "../context/user/UserContext.js";
+import useFetch from "../hooks/useFetch.js";
 
 export default function EditPostPage() {
   const { id } = useParams();
-  const { state: user } = useContext(StateContext);
+  const { user } = useContext(UserContext);
   const [post, setPost] = useState(null);
 
   // GET hook for fetching post
-  const { isLoading, error, performFetch, cancelFetch } = useFetchWithAuth(
+  const { performFetch, cancelFetch } = useFetch(
     `/post/${id}`,
     (res) => setPost(res.post ?? res),
   );
@@ -22,14 +21,11 @@ export default function EditPostPage() {
     return () => cancelFetch();
   }, [id]);
 
-  useSetError(error);
-
   // Author check
   if (!user || !user.userId) return <div>Loading user…</div>;
   if (post && post.author && post.author._id !== user.userId)
     return <div>Not authorized</div>;
 
-  if (isLoading && !post) return <div>Loading…</div>;
   if (!post) return null;
 
   return <EditPostForm post={post} />;
